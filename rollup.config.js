a14e1da6e924ca327a6a562ad695dc328407aaaa
nikhilkuyya <nikhilkuyya@gmail.com>
2020-06-26 10:23:43 +0530

    add view

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import nodeResolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript';
 import multiInput from 'rollup-plugin-multi-input';
 import serve from 'rollup-plugin-serve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default [{
     input: ["./samples/**/index.ts"],
@@ -9,7 +10,7 @@ export default [{
       dir: './',
       format: 'cjs'
     }],
-    plugins : [nodeResolve(),typescript(),multiInput(),serve({
+    plugins : [nodeResolve(),commonjs(), typescript(),multiInput(),serve({
       open : true,
       host: 'localhost',
       port : 12345,
--- samples/mouse-event-rxjs/index.ts
+++ samples/mouse-event-rxjs/index.ts
@@ -1,17 +1,28 @@
 import {fromEvent } from 'rxjs';
 import { filter, tap, takeUntil, map, concatMap } from 'rxjs/operators';
+import * as ColorPk from 'color';
 
 const click = fromEvent(document,'click').pipe(tap(console.log)).subscribe()
+const Color = ColorPk.default;
 
 const mouseDown = fromEvent(document,'mousedown');
 const mouseUp = fromEvent(document,'mouseup');
 const mouseMove = fromEvent(document,'mousemove');
+const documentBody = document.getElementsByTagName('body')[0];
 
 const mouseMoveSubscription = mouseDown.pipe(
     concatMap(event => {
        return  mouseMove.pipe(takeUntil(mouseUp));
     }),tap((evt : MouseEvent) => {
-        console.log('event',evt.clientX,evt.clientY);
+        const element = document.createElement('div');
+        const x = evt.clientX;
+        const y = evt.clientY;
+        element.innerText = `x : ${x}, y : ${y}`;
+        documentBody.append(element);
+        const colorValue = Color(`rgb(${x%255},${(y%255)},${(x*y)%255})`);
+        const hex = colorValue.hex();
+        const backgroundColor = `background-color  : ${hex}`;
+        documentBody.setAttribute('style',backgroundColor);
     })
 ).subscribe();
 
