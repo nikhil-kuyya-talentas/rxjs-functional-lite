1a006cf3aa326a9764c0673a42e3ead5af404643
Nikhil Kuyya <nikhil.k@talentas.in>
2020-07-06 09:10:43 +0530

    base mouse events

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,31 @@
-import nodeResolve from '@rollup/plugin-node-resolve'
-import typescript from '@rollup/plugin-typescript';
-import multiInput from 'rollup-plugin-multi-input';
-import serve from 'rollup-plugin-serve';
-import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import multiInput from "rollup-plugin-multi-input";
+import serve from "rollup-plugin-serve";
+import commonjs from "rollup-plugin-commonjs";
 
-export default [{
-    input: ["./samples/**/index.ts"],
-    output: [{
-      dir: './',
-      format: 'cjs'
-    }],
-    plugins : [nodeResolve(),commonjs(), typescript(),multiInput(),serve({
-      open : true,
-      host: 'localhost',
-      port : 12345,
-      verbose : true,
-      contentBase : 'samples'      
-    })]
-}
+export default [
+  {
+    input: "./samples/mouse-event/index.ts",
+    output: [
+      {
+        dir: "./samples/mouse-event/",
+        entryFileNames: "index.js",
+        format: "cjs",
+      },
+    ],
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      typescript(),
+      multiInput(),
+      serve({
+        open: true,
+        host: "localhost",
+        port: 12345,
+        verbose: true,
+        contentBase: "samples/mouse-event",
+      }),
+    ],
+  },
 ];
-
--- samples/mouse-event/index.ts
+++ samples/mouse-event/index.ts
@@ -1,23 +1,36 @@
-document.onreadystatechange = function(ev) {
+import * as ColorPk from "color";
+const Color = ColorPk.default;
 
-    function complete() {
-        document.addEventListener('mousedown',function() {
-            document.addEventListener('mousemove',mouseMoveEventHanlder);
-            document.addEventListener('mouseup', function(evt) {
-                document.removeEventListener('mousemove',mouseMoveEventHanlder);
-            })
-        })
-        
-        function mouseMoveEventHanlder(evt : MouseEvent) {            
-            console.log(evt.clientX,evt.clientY);
-        }
-        document.addEventListener('click',console.log);
+document.onreadystatechange = function (ev) {
+  function complete() {
+    document.addEventListener("mousedown", function () {
+      document.addEventListener("mousemove", mouseEventHandler);
+      document.addEventListener("mouseup", function (evt) {
+        document.removeEventListener("mousemove", mouseEventHandler);
+      });
+    });
+
+    // function mouseMoveEventHanlder(evt: MouseEvent) {
+    //   console.log(evt.clientX, evt.clientY);
+    // }
+
+    function mouseEventHandler(evt: MouseEvent): void {
+      const element = document.createElement("div");
+      const x = evt.clientX;
+      const y = evt.clientY;
+      element.innerText = `x : ${x}, y : ${y} - ${evt.type}`;
+      documentBody.append(element);
+      const colorValue = Color(`rgb(${x % 255},${y % 255},${(x * y) % 255})`);
+      const hex = colorValue.hex();
+      const backgroundColor = `background-color  : ${hex}`;
+      documentBody.setAttribute("style", backgroundColor);
     }
+    document.addEventListener("click", console.log);
+  }
 
-    if(document.readyState === 'complete'){
-        console.log('ready');
-        complete();
-    }
-}
-
-
+  if (document.readyState === "complete") {
+    console.log("ready");
+    complete();
+    var documentBody = document.getElementsByTagName("body")[0];
+  }
+};
